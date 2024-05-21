b78347cb2d2576dcace0b82b97e2f5f762ba3e86
cse23-116 <159142874+cse23-116@users.noreply.github.com>
2024-05-21 23:13:53 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+ /*Array of feedback messages*/
+      var feedbacks = [
+        "Great job! Your feeback is much appreciated.",
+        "Thank you for your valuable input!",
+        "We appreciate your thoughts!"
+      ];
+
+      //Function to display a random feedback message
+      function displayRandomFeedback() {
+        var randomIndex= Math.floor(Math.random() * feedbacks.length);
+        var feedback = feedbacks[randomIndex];
+        var feedbackContainer = document.getElementById("feedback-container");
+        feedbackContainer.innerHTML = feedback;
+      }
+      // Call the function to display a random message on page load
+  displayRandomFeedback();
+      
